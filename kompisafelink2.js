8d7688a6b47470c1b470e2be97640405d8eca0c5
Jaguring <ronnymultimedia@gmail.com>
2017-02-14 02:39:29 +0700

    Add files via upload

--- kompisafelink2.js
+++ kompisafelink2.js
@@ -26,3 +26,4 @@ if(a_to_vk)
 function a_to_fa()
 {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
 protected_links = "kandangfiles.wapka.mobi,vcd-minang.club,goo.gl,adreactor.com,tubedown.nextwapblog.com,google.com";
+auto_safelink();
